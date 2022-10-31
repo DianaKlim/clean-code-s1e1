1916d3f5bd00b132ae8b04c08b04b9e6a4e34189
Diana <diana.klimsmm@gmail.com>
2022-10-31 10:54:07 +0300

    refactor: rename id, classes from css and js style to rule 3.2, 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
     console.log("Add Task...");
-    //Create a new list item with the text from the #new__task:
+    //Create a new list item with the text from the #new_task:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
 
